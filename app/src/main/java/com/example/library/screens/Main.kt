107e815b53c80dfc5fe03c13ba17a90c8aa5db11
Норден <kirilleven@mail.ru>
2023-06-28 01:21:46 +0300

    making main screen

--- app/src/main/java/com/example/library/screens/Main.kt
+++ app/src/main/java/com/example/library/screens/Main.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,7 +51,10 @@ fun MainScreen(navController: NavHostController) {
                     .fillMaxSize(),
                 color = AppBackground
             ) {
-
+                Text(text = "Основное",
+                    color = Color.Black,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 14.sp)
             }
 
         }
